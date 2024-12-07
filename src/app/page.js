4e3aed0b1977535d3829721a60392e124cc836bc
Diane <dianekim120@gmail.com>
2024-12-07 18:25:20 -0500

    chore: final edits + adjustments

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
       <Header />
       <Landing />
       <Experience />
-      <PersonalProjects />
+      {/* <PersonalProjects /> */}
       <Footer />
     </div>
   );
