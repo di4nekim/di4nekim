35bff7e16cfd6ed11fbe44c50bb107c9e32c2e60
Diane <dianekim120@gmail.com>
2024-12-08 04:20:44 -0500

    add: favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/sparkles.svg"></link>
+      </head>
       <SpeedInsights />
       <body
         className={inter.variable}
